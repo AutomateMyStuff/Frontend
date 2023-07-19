55ab3ba923f1d00a319122303869a5de52795a78
Earnest Angel <fareast.sn@gmail.com>
2023-07-19 11:12:21 +0800

    0.2.0

---
+++ cucumber.js
@@ -0,0 +1,5 @@
+require("custom-env").env(true);
+
+module.exports = {
+  default: `--format-options '{"snippetInterface": "synchronous"}'`,
+};
---
+++ src/components/SignedIn/index.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { SignedIn } from './index'
+
+jest.mock("next-auth/react", () => {
+    const originalModule = jest.requireActual('next-auth/react');
+    const mockSession = {
+        expires: new Date(Date.now() + 2 * 86400).toISOString(),
+        user: { username: "admin" }
+    };
+    return {
+        __esModule: true,
+        ...originalModule,
+        useSession: jest.fn(() => {
+            return { data: mockSession, status: 'authenticated' }  // return type is [] in v3 but changed to {} in v4
+        }),
+    };
+});
+
+describe('Components/SignedIn', () => {
+    it('renders SignIn', () => {
+        const { container } = render(<SignedIn />);
+        expect(container).toMatchSnapshot();
+    })
+
+    it('Should see the header in the screen', () => {
+        render(<SignedIn />);
+        expect(screen.getByRole('heading', { name: "Signed In" })).toBeInTheDocument();
+    });
+
+    it('Should see the Logout button', () => {
+        render(<SignedIn />);
+        expect(screen.getByRole('button', { name: "Sign out" })).toBeInTheDocument();
+    });
+});
