338ca392eff2d385ce73a3a4265490747600a967
Earnest Angel <fareast.sn@gmail.com>
2023-07-19 22:33:17 +0800

    Change URL of Playwright test

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://automate-my-stuff-frontend-dev-5b2e6dc9d29e.herokuapp.com/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Next Social Login/);
 });
 
 test('Sign in with Github', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://automate-my-stuff-frontend-dev-5b2e6dc9d29e.herokuapp.com/');
 
   // Click the get started link.
   //await page.getByRole('link', { name: 'Images' }).click();
