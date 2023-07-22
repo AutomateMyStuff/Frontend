e5d3d2462a1450e020789dc266d0658253cec8ec
Earnest Angel <fareast.sn@gmail.com>
2023-07-22 19:43:21 +0800

    Add K6 load testing demo

---
+++ loadtests/performance-test.js
@@ -0,0 +1,27 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
+export const options = {
+  duration: '.5m',
+  vus: 5,
+  iterations: 10,
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
+};
+
+export default function () {
+  http.get('http://test.k6.io/contacts.php');
+  sleep(3);
+}
+
+export function handleSummary(data) {
+  console.log('Finished executing performance tests');
+
+  return {
+    'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+    'summary.json': JSON.stringify(data), // and a JSON with all the details...
+  };
+}
